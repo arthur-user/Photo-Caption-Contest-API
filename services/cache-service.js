697de47c0c705e3d7b6dd1477491754affcd6ee8
robotx48 <robotx48@gmail.com>
2025-02-26 23:44:28 -0800

    Fixed the issue with route handler authentication and readied Render deployment

--- services/cache-service.js
+++ services/cache-service.js
@@ -1,30 +1,29 @@
-// https://medium.com/@danielsternlicht/caching-like-a-boss-in-nodejs-9bccbbc71b9b
 const NodeCache = require('node-cache');
 
 module.exports = class CacheService {
-
-  constructor(ttlSeconds) {
-    this.cache = new NodeCache({ stdTTL: ttlSeconds, checkperiod: ttlSeconds * 0.2, useClones: false });
-  }
-
-  get(key, storeFunction) {
-    const value = this.cache.get(key);
-    if (value) {
-      console.log(`Retrieving ${key} from cache`);
-      return Promise.resolve(value);
+    constructor(ttlSeconds){
+        this.cache = new NodeCache({ stdTTL: ttlSeconds, checkperiod: ttlSeconds * 0.2, useClones: false}); 
     }
 
-    return storeFunction().then((result) => {
-      this.cache.set(key, result);
-      return result;
-    });
-  }
+    get(key, storeFunction){
+        const value = this.cache.get(key);
+        if(value){
+            console.log(`Getting ${key} from cache`);
+            return Promise.resolve(value);
+        }
 
-  delete(keys) {
-    this.cache.del(keys);
-  }
+        return storeFunction().then((result) => {
+            this.cache.set(key,result);
+            return result;
+        });
+    }
 
-  flush() {
-    this.cache.flushAll();
-  }
+    delete(keys){
+        this.cache.del(keys);
+    }
+
+    flush(){
+        this.cache.flushAll();
+    }
+
 };
--- services/caption-service.js
+++ services/caption-service.js
@@ -1,5 +1,5 @@
 const CacheService = require('./cache-service');
-const cache = new CacheService(3600); // cache 1 hour
+const cache = new CacheService(1800); // cache 30 minutes
 const CACHE_KEY = 'caption';
 
 const Photo = require('../models').Photo;
@@ -7,90 +7,100 @@ const Caption = require('../models').Caption;
 const User = require('../models').User;
 
 module.exports = {
-  getById(req, res) {
-    return cache.get(`${CACHE_KEY}_${req.params.id}`, () => Caption
-      .findByPk(req.params.id, {
-        include: [{
-          model: Photo,
-          as: 'photo'
-        }, {
-          model: User,
-          as: 'user'
-        }]
-      }))
-      .then((caption) => {
-        if (!caption) {
-          return res.status(404).send({
-            message: 'Caption Not Found',
-          });
-        }
-        return res.status(200).send(caption);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(400).send(error);
-      });
-  },
+    getById(req, res) { //.get method of the CacheService module
+        return cache.get(`${CACHE_KEY}_${req.params.id}`, () => 
+            Caption.findByPk(req.params.id, {
+                include: [
+                    {
+                        model: Photo,
+                        as: 'photo'
+                    },
+                    {
+                        model: User,
+                        as: 'user'
+                    }
+                ]
+            })
+        )
+        .then((caption) => {
+            if (!caption) {
+                return res.status(404).send({
+                    message: 'Caption not present',
+                });
+            }
+            return res.status(200).send(caption);
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(400).send({ message: 'An error occurred during transaction' });
+        });
+    },
 
-  add(req, res) {
-    const userId = req.user.id;
-    return Caption
-      .create({
-        photo_id: req.body.photo_id,
-        user_id: userId,
-        comment: req.body.comment
-      })
-      .then((caption) => res.status(201).send(caption))
-      .catch((error) => res.status(400).send(error));
-  },
+    add(req, res) {
+        const userId = req.user.id;
+        return Caption
+            .create({
+                photo_id: req.body.photo_id,
+                user_id: userId,
+                comment: req.body.comment
+            })
+            .then((caption) => res.status(201).send(caption))
+            .catch((error) => {
+                console.error(error);
+                res.status(400).send({ message: 'An error occurred during transaction' });
+            });
+    },
 
-  update(req, res) {
-    return Caption
-      .findByPk(req.params.id)
-      .then(caption => {
-        if (!caption) {
-          return res.status(404).send({
-            message: 'Caption Not Found',
-          });
-        }
-        if (caption.user_id !== req.user.id) {
-          return res.status(403).send({
-            message: 'User not authorized to update this caption.'
-          })
-        }
+    update(req, res) {
+        return Caption
+            .findByPk(req.params.id)
+            .then((caption) => {
+                if (!caption) {
+                    return res.status(404).send({
+                        message: 'Requested caption not found',
+                    });
+                }
+                if (caption.user_id !== req.user.id) {
+                    return res.status(403).send({
+                        message: 'User has unauthorized/lacking permissions'
+                    });
+                }
 
-        return caption
-          .update({
-            comment: req.body.comment || caption.comment
-          })
-          .then(() => cache.delete(`${CACHE_KEY}_${req.params.id}`))
-          .then(() => res.status(200).send(caption))
-          .catch((error) => res.status(400).send(error));
-      })
-      .catch((error) => res.status(400).send(error));
-  },
+                return caption
+                    .update({
+                        comment: req.body.comment || caption.comment
+                    })
+                    .then(() => cache.delete(`${CACHE_KEY}_${req.params.id}`))
+                    .then(() => res.status(200).send(caption))
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(400).send({ message: 'An error occurred during transaction' });
+                    });
+            });
+    },
 
-  delete(req, res) {
-    return Caption
-      .findByPk(req.params.id)
-      .then(caption => {
-        if (!caption) {
-          return res.status(400).send({
-            message: 'Caption Not Found',
-          });
-        }
-        if (caption.user_id !== req.user.id) {
-          return res.status(403).send({
-            message: 'User not authorized to delete this caption.'
-          })
-        }
-
-        return caption
-          .destroy()
-          .then(() => cache.delete(`${CACHE_KEY}_${req.params.id}`))
-          .then(() => res.status(204).send())
-          .catch((error) => res.status(400).send(error));
-      })
-      .catch((error) => res.status(400).send(error));
-  },
+    delete(req, res){
+        return Caption
+        .findByPk(req.params.id)
+        .then(caption => {
+            if(!caption){
+                return res.status(400).send({
+                    message: 'Requested caption not found',
+                });
+            }
+            if (caption.user_id !== req.user.id){
+                return res.status(403).send({
+                    message: 'User has unauthorized/lacking permissions'
+                })
+            }
+            return caption
+            .destroy()
+            .then(() => cache.delete(`${CACHE_KEY}_${req.params.id}`))
+            .then(() => res.status(204).send())
+            .catch((error) => {
+                console.error(error);
+                res.status(400).send({ message: 'An error occurred during transaction'});
+            });
+        });
+    },
 };
